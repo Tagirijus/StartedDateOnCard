529b507eb0f7c248eafcfee562f1e7cadd20ee2d
Manuel <info@tagirijus.de>
2023-03-11 16:11:51 +0100

    More meta data

--- Plugin.php
+++ Plugin.php
@@ -12,56 +12,27 @@ class Plugin extends Base
     public function initialize()
     {
         // Template Override
-        //  - Override name should be camelCase e.g. pluginNameExampleCamelCase
-        $this->template->setTemplateOverride('action/index', 'pluginNameExampleCamelCase:action/index');
-
-        // CSS - Asset Hook
-        //  - Keep filename lowercase
-        $this->hook->on('template:layout:css', array('template' => 'plugins/PluginNameExampleStudlyCaps/Assets/css/plugin-name.css'));
-
-        // JS - Asset Hook
-        //  - Keep filename lowercase
-        $this->hook->on('template:layout:js', array('template' => 'plugins/PluginNameExampleStudlyCaps/Assets/js/plugin-name.js'));
+        // $this->template->setTemplateOverride('action/index', 'pluginNameExampleCamelCase:action/index');
 
         // Views - Template Hook
-        //  - Override name should start lowercase e.g. pluginNameExampleCamelCase
-        $this->template->hook->attach('template:project-header:view-switcher-before-project-overview', 'pluginNameExampleCamelCase:project_header/actions');
-
-        // Views - Add Menu Item - Template Hook
-        //  - Override name should start lowercase e.g. pluginNameExampleCamelCase
-        //  - Example for menu item in kanboard settings page: $this->template->hook->attach('template:config:sidebar', 'pluginNameExampleCamelCase:config/sidebar');
-
-        // Extra Page - Routes
-        //  - Example: $this->route->addRoute('/my/custom/route', 'MyController', 'show', 'PluginNameExampleStudlyCaps');
-        //  - Must have the corresponding action in the matching controller
-        $this->route->addRoute('/ / ', ' ', ' ', 'PluginNameExampleStudlyCaps');
-
-        // Helper
-        //  - Example: $this->helper->register('helperClassNameCamelCase', '\Kanboard\Plugin\PluginNameExampleStudlyCaps\Helper\HelperNameExampleStudlyCaps');
-        //  - Add each Helper in the 'use' section at the top of this file
-        $this->helper->register(' ', '\Kanboard\Plugin\  \Helper\  ');
-    }
-
-    public function onStartup()
-    {
-        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
+        $this->template->hook->attach('template:board:task:footer', 'TagiStartedDateOnCard:project_header/actions');
     }
 
     public function getPluginName()
     {
         // Plugin Name MUST be identical to namespace for Plugin Directory to detect updated versions
         // Do not translate the plugin name here
-        return 'PluginNameExampleStudlyCaps';
+        return 'TagiStartedDateOnCard';
     }
 
     public function getPluginDescription()
     {
-        return t('description text');
+        return t('This plugin adds a visual hint on a card, if it has a started date.');
     }
 
     public function getPluginAuthor()
     {
-        return '';
+        return 'Tagirijus';
     }
 
     public function getPluginVersion()
@@ -75,11 +46,11 @@ class Plugin extends Base
         // >=1.0.37
         // <1.0.37
         // <=1.0.37
-        return '>=1.2.20';
+        return '>=1.2.27';
     }
 
     public function getPluginHomepage()
     {
-        return 'https://github.com/ /url';
+        return 'https://github.com/Tagirijus/kanboard-TagiStartedDateOnCard';
     }
 }
