98406102c6fd8fff074451d8d37c5ac8f924519b
Manuel <info@tagirijus.de>
2023-03-12 12:37:59 +0100

    Date without time

--- Template/board/task_footer_started_date.php
+++ Template/board/task_footer_started_date.php
@@ -4,11 +4,7 @@
         <div class="task-board-icons-row" style="margin-top: -.5em; margin-bottom: -.5em;">
             <span title="<?= t('Start Date') ?>">
                 <i class="fa fa-play" role="img" aria-label="<?= t('Start Date') ?>"></i>
-                <?php if (date('Hi', $task['date_started']) === '0000' ): ?>
-                    <?= $this->dt->date($task['date_started']) ?>
-                <?php else: ?>
-                    <?= $this->dt->datetime($task['date_started']) ?>
-                <?php endif ?>
+                <?= $this->dt->date($task['date_started']) ?>
             </span>
         </div>
     </div>
--- changelog.md
+++ changelog.md
@@ -1,6 +1,14 @@
 # Changelog
 
 
+## v1.1
+
+### What's Changed
+
+_(most recent changes are listed on top):_
+- Date without time now
+
+
 ## v1.0
 
 ### What's Changed
